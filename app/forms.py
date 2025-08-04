96772b18220e196ba34c3577be9d8348fb7c66c3
Rishi Raj <rishiraj14062003@gmail.com>
2025-08-05 01:26:10 +0530

    files loaded

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comment
+        fields=('name','email','body')
+        
+        widgets={
+            'name':forms.TextInput(attrs={'class':'form-control','placeholder':'Enter your name'}),
+            'email':forms.EmailInput(attrs={'class':'form-control','placeholder':'Enter your email'}),
+            'body':forms.Textarea(attrs={'class':'form-control','placeholder':'Enter your comment'})
+        }
---
+++ app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import blog_list, blog_detail,post_like,search
+
+
+urlpatterns=[
+    path('',blog_list,name='list'),
+    path('posts/<int:pk>/',blog_detail,name='detail'),
+    path('like_posts/<int:pk>/',post_like,name='like_posts'),
+    path('post/search_results/',search,name='search')
+]
